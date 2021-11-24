34cf9ce69a10f7ee4eea66f4858cbdcca6e725d5
C2k-DEV <itseni@dandabs.fi>
2021-11-24 11:46:01 +0000

    Reading data from OleDbConnection in splash

--- WindowsFormsApp1/frmSplash.Designer.cs
+++ WindowsFormsApp1/frmSplash.Designer.cs
@@ -29,13 +29,18 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.lblTitle = new System.Windows.Forms.Label();
             this.pnlItems = new System.Windows.Forms.FlowLayoutPanel();
             this.btnQuiz = new System.Windows.Forms.Button();
             this.btnPupils = new System.Windows.Forms.Button();
             this.tmpButton = new System.Windows.Forms.Button();
             this.lblSubtitle = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.pnlItems.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -98,11 +103,30 @@ namespace WindowsFormsApp1
             this.lblSubtitle.Text = "Please select a task.";
             this.lblSubtitle.Click += new System.EventHandler(this.lblSubtitle_Click);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(299, 56);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(359, 360);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
             // frmSplash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.lblSubtitle);
             this.Controls.Add(this.pnlItems);
             this.Controls.Add(this.lblTitle);
@@ -110,6 +134,7 @@ namespace WindowsFormsApp1
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.frmSplash_Load);
             this.pnlItems.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,5 +148,6 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button btnPupils;
         private System.Windows.Forms.Button tmpButton;
         private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- WindowsFormsApp1/frmSplash.cs
+++ WindowsFormsApp1/frmSplash.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsFormsApp1
 {
@@ -24,11 +26,35 @@ namespace WindowsFormsApp1
 
         private void frmSplash_Load(object sender, EventArgs e)
         {
+            // update tile to time of day + session user's uname/uid
             int intHourNow = System.DateTime.Now.Hour;
             if (intHourNow >= 20) lblTitle.Text = "Good night, " + Program.sessionUser + ".";
             if (intHourNow >= 16) lblTitle.Text = "Good evening, " + Program.sessionUser + ".";
             if (intHourNow >= 12) lblTitle.Text = "Good afternoon, " + Program.sessionUser + ".";
             if (intHourNow >= 0) lblTitle.Text = "Good morning, " + Program.sessionUser + ".";
+
+            // update chart data from pupils file
+            
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\SkiSlope";
+            string file = "pupils.txt";
+
+            string conStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+    path + ";Extended Properties=\"Text;HDR=No;FMT=Delimited\"";
+            OleDbConnection conn = new OleDbConnection(conStr);
+
+            string selectQr = "Select * from " + file;
+            OleDbCommand cmd = new OleDbCommand(selectQr, conn);
+
+            cmd.Connection.Open();
+            OleDbDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            chart1.Series[0].Points.DataBindXY(reader, "0", reader, "2");
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+
+            reader.Close();
+            conn.Close();
+
+
         }
 
         private void btnQuiz_Click(object sender, EventArgs e)
@@ -41,5 +67,13 @@ namespace WindowsFormsApp1
             Form frmPupils = new frmPupils();
             frmPupils.ShowDialog();
         }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
